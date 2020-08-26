aa060cd3c934893ba1afd5995454e41ba01834d3
Afonso Machado <afonso777@hotmail.com>
2020-08-26 16:24:00 -0300

    corrigindo bug com o total de itens

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,17 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
 
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  async function loadPage(pageNumber = page, shootRefresh) {
+  async function loadPage(pageNumber = page, shouldRefresh) {
     if (total && pageNumber > total) {
       return;
     }
+    setLoading(true);
 
     const url = `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`;
     // Carregando os itens do feed do backend
@@ -19,13 +29,14 @@ export default function Feed() {
     // convertendo os dados em json
     const data = await resp.json();
     // Capturando total de elementos
-    const totalItems = resp.headers.get('X-Total_Count');
+    const totalItems = resp.headers.get('X-Total-Count');
     // armazenando os dados
     // Numero de paginas, arredondandos para cima
     setTotal(Math.floor(totalItems / 5));
     // incrementando dados no feet ao inves de substituir os dados
     setFeed([...feed, ...data]);
     setPage(pageNumber + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -41,6 +52,8 @@ export default function Feed() {
         onEndReached={() => loadPage()}
         // quando o usuario tiver 10% do final da lista, carrega os proximos itens
         onEndReachedThreshold={0.1}
+        // ultimo item da lista
+        ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
             <Header>

2a82fe3ee43b05cf9b729c15be17a91fecf766ca
Afonso Machado <afonso777@hotmail.com>
2020-08-26 17:11:35 -0300

    transição da imagem com blur para a imagem original

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Animated} from 'react-native';
 
 import {Small, Original} from './styles';
 
+const OriginalAmimated = Animated.createAnimatedComponent(Original);
+
 export default function LazyImage({smallSource, source, aspectRatio}) {
+  // se a imagem original foi carregada ou nao
+  const [loaded, setLoaded] = useState(false);
+
+  // Dando um intervalode 1 segundo para visualizar o carregamento da imagem
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
+
   function handleAnimate() {}
 
   return (
@@ -11,12 +24,14 @@ export default function LazyImage({smallSource, source, aspectRatio}) {
       ratio={aspectRatio}
       resizeMode="contain"
       blurRadius={2}>
-      <Original
-        source={source}
-        ratio={aspectRatio}
-        resizeMode="contain"
-        onLoadEnd={handleAnimate}
-      />
+      {loaded && (
+        <OriginalAmimated
+          source={source}
+          ratio={aspectRatio}
+          resizeMode="contain"
+          onLoadEnd={handleAnimate}
+        />
+      )}
     </Small>
   );
 }

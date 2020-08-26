23fff061a32f5dc629af0ad37750fdc505e0bdef
Afonso Machado <afonso777@hotmail.com>
2020-08-26 17:02:19 -0300

    iniciando função de animação para transição da imagem small para original

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
 
-import {Small} from './styles';
+import {Small, Original} from './styles';
 
 export default function LazyImage({smallSource, source, aspectRatio}) {
+  function handleAnimate() {}
+
   return (
     <Small
       source={smallSource}
       ratio={aspectRatio}
       resizeMode="contain"
-      blurRadius={2}
-    />
+      blurRadius={2}>
+      <Original
+        source={source}
+        ratio={aspectRatio}
+        resizeMode="contain"
+        onLoadEnd={handleAnimate}
+      />
+    </Small>
   );
 }
--- src/components/LazyImage/styles.js
+++ src/components/LazyImage/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
 
-export const Small = styled.Image`
+export const Small = styled.ImageBackground`
+  width: 100%;
+  /* proipriedade css do ratio */
+  aspect-ratio: ${(props) => props.ratio};
+`;
+
+export const Original = styled.Image`
   width: 100%;
   /* proipriedade css do ratio */
   aspect-ratio: ${(props) => props.ratio};
